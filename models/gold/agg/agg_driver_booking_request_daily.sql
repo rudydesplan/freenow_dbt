82be90f5790749e9a63fa433d1d499cc974703a2
rudy desplan <desrudy@hotmail.com>
2026-02-23 14:13:19 +0100

    Add files via upload

--- models/gold/agg/agg_driver_booking_request_daily.sql
+++ models/gold/agg/agg_driver_booking_request_daily.sql
@@ -9,11 +9,13 @@ select
     accepted_driver_id as driver_id,
     date_key,
 
-    count(*)::bigint as bookings_assigned_count_request_anchor,
-    sum(estimated_route_fare_eur)::double precision as sum_estimated_fare_eur_request_anchor,
-    avg(estimated_route_fare_eur)::double precision as avg_estimated_fare_eur_request_anchor,
+    count(*)::bigint as bookings_assigned_count,
+    sum(estimated_route_fare_eur)::double precision as sum_estimated_fare_eur,
+    avg(estimated_route_fare_eur)::double precision as avg_estimated_fare_eur,
 
-    current_timestamp as _processed_at
+    current_timestamp as _processed_at,
+    '{{ invocation_id }}' as _dbt_invocation_id,
+    NULL::varchar as _openlineage_run_id
 
 from b
 where accepted_driver_id is not null
